ec86ccc00a91c13266c1381ea212f72de1d16136
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-10-02 14:20:40 -0500

    bottons and slider

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 var password = "";
 
-//set password length to slider value
+//show slider value
+
+
+document.getElementById("length").innerHTML = "Password Length: " + "" + slider.value;
+
 document.getElementById("slider").oninput = function () {
-    document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
+    document.getElementById("length").innerHTML = "Password Length: " + slider.value;
 }
 
-//set default length display of 10
-document.getElementById("length").innerHTML = "length: 10";
 
 //generate random password
 function generate() {
-    var passLength = document.getElementById("slider").value;
+    var passLength = (document.getElementById("slider").value);
     for (var i = 0; i < passLength; i++) {
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
     }
-
     //add password to textbox/display area
     document.getElementById("displayPassword").value = password;
-
     console.log(password);
-
 }
+
 
 
 //function to copy password to clipboard
