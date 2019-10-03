50670bc85c7199053458993a9b839caf53272c63
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-10-03 13:43:33 -0500

    add event listener for special characters

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 //password variables 
-var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+var noSpecial = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890"
+var special = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 var password = "";
+var checkbox = document.getElementById("specialChar")
+var mode = "true"
 
 //set slider value
 document.getElementById("slider").oninput = function () {
     document.getElementById("length").textContent = "Password Length: " + slider.value;
 }
 
+//add event listener for specialChar
+checkbox.addEventListener("click", function () {
+    if (mode === "true") {
+        mode = "false";
+        checkbox.setAttribute("value", "false");
+    } else {
+        mode = "true";
+        checkbox.setAttribute("value", "true");
+    }
+    console.log(mode)
+});
+
 
 //generate random password
 function generate() {
-    password = "";
-    var passLength = (document.getElementById("slider").value);
-    for (var i = 0; i < passLength; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
+    if (mode === "true") {
+        password = "";
+        var passLength = (document.getElementById("slider").value);
+        for (var i = 0; i < passLength; i++) {
+            password = password + special.charAt(Math.floor(Math.random() * Math.floor(special.length)));
+        }
+        //add password to textbox/display area
+        document.getElementById("displayPassword").value = password;
+
+    } else if (mode === "false") {
+        password = "";
+        var passLength = (document.getElementById("slider").value);
+        for (var i = 0; i < passLength; i++) {
+            password = password + noSpecial.charAt(Math.floor(Math.random() * Math.floor(noSpecial.length)));
+        }
+
     }
     //add password to textbox/display area
     document.getElementById("displayPassword").value = password;
     console.log(password);
     console.log(slider.value);
+    console.log(mode);
 }
 
 
